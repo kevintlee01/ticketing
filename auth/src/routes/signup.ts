52da8f6845bbf4f4451e4622a2b9f2b9d4156bb5
Kevin Lee <kevinlee3618@gmail.com>
2022-09-03 16:05:49 -0700

    Add cookies & jsonwebtokens

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
@@ -31,6 +33,21 @@ router.post('/api/users/signup', [
 
   const user = User.build({ email, password });
   await user.save();
+
+  // Generate JWT
+  // kubectl create secret generic jwt-secret --from-literal=JWT_KEY=fake_secret
+  const userJwt = jwt.sign(
+    {
+      id: user.id,
+      email: user.email
+    },
+    process.env.JWT_KEY!
+  );
+
+  // Store it in session object
+  req.session = {
+    jwt: userJwt
+  };
 
   res.status(201).send(user);
 });
