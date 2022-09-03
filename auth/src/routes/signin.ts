eb61dfd54624bd205ebc1896bafa7aa57bc71b36
Kevin Lee <kevinlee3618@gmail.com>
2022-09-03 16:46:21 -0700

    Implement Sign In

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,60 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send("Hello signin");
-});
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    // kubectl create secret generic jwt-secret --from-literal=JWT_KEY=fake_secret
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it in session object
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
