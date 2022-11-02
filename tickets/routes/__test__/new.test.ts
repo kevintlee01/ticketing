9e6cf79eaf9d3e66f96d25c4b9570b4b2ccff951
Kevin Lee <kevinlee3618@gmail.com>
2022-11-02 07:41:56 -0700

    Add template for create ticket router

---
+++ tickets/routes/__test__/new.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+
+import { app } from "../../src/app";
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .send({});
+
+  expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed when user is signed in', async () => {
+
+});
+
+it('returns an error if an invalid title is provided', async () => {
+
+});
+
+it('returns an error if an invalid price is provided', async () => {
+
+});
+
+it('creates a ticket with valid inputs', async () => {
+
+});
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { errorHandler, NotFoundError } from '@ktltickets/common';
+import { createTicketRouter } from '../routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +15,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 );
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
