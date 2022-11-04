7caec4c8d1727be4624e9f62b642392a952862ee
Kevin Lee <kevinlee3618@gmail.com>
2022-11-04 07:53:36 -0700

    add ticket validation

--- tickets/routes/__test__/new.test.ts
+++ tickets/routes/__test__/new.test.ts
@@ -28,11 +28,42 @@ it('returns status other than 401 if user is signed in', async () => {
 });
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test_title',
+      price: -10
+    })
+    .expect(400);
 
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'test_title',
+      price: 10
+    })
+    .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
--- tickets/routes/new.ts
+++ tickets/routes/new.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
-import { requireAuth } from '@ktltickets/common';
+import { requireAuth, validateRequest } from '@ktltickets/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth, [
+  body('title')
+    .not()
+    .isEmpty()
+    .withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0')
+], validateRequest, (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
