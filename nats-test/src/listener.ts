1aed3ffb6db6ba496eb73856920728d0f3155ae9
Kevin Lee <kevinlee3618@gmail.com>
2022-11-14 07:58:45 -0800

    add queue groups

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -17,11 +17,13 @@ stan.on('connect', () => {
 
   const options = stan
     .subscriptionOptions()
-    .setManualAckMode(true);
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('order-service');
 
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group',
+    'queue-group-name',
     options
   );
 
