ea447cc8859e99b892727b1eb3da67773bc4da62
Kevin Lee <kevinlee3618@gmail.com>
2022-11-19 09:33:16 -0800

    add ticket updated publisher

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -8,6 +8,8 @@ import {
   NotAuthorizedError
 } from '@ktltickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -41,6 +43,13 @@ router.put(
     });
 
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
 
     res.send(ticket);
   });
