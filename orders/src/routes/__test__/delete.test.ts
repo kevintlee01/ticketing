89e84f5810e8657748617701cb9edcc5d53ba353
Kevin Lee <kevinlee3618@gmail.com>
2022-11-24 09:50:06 -0800

    implement order routes with unit tests

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('marks an order as cancelled', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+
+  await ticket.save();
+
+  const user = global.signin();
+
+  // make a request to build an order with ticket created
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to cancel order
+  const { body: fetchedOrder } = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // make sure order is cancelled 
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,34 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+
+import {
+  requireAuth,
+  BadRequestError,
+  NotFoundError,
+  NotAuthorizedError
+} from '@ktltickets/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    throw new BadRequestError('Order ID is invalid');
+  }
+
+  const order = await Order.findById(orderId).populate('ticket');
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
