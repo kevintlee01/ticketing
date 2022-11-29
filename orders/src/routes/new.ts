d2f2e351e58dd8d864d39a24e7918c547d6f5f3c
Kevin Lee <kevinlee3618@gmail.com>
2022-11-29 08:32:54 -0800

    add versioning to ticketing

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -63,6 +63,7 @@ router.post(
     // publish event order was created
     new OrderCreatedPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       status: order.status,
       userId: order.userId,
       expiresAt: order.expiresAt.toISOString(),
