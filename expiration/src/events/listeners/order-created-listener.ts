1b970b182d37e5af3a1ea015d8e314413c99e8f8
Kevin Lee <kevinlee3618@gmail.com>
2022-12-09 07:57:24 -0800

    add expiration queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,12 +2,17 @@ import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@ktltickets/common";
 
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id
+    });
 
+    msg.ack();
   }
 }
