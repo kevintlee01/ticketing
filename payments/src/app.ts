4bbd70e42996b82f75e82cc34cb01fdad45afc88
Kevin Lee <kevinlee3618@gmail.com>
2023-01-06 08:42:31 -0800

    add payments router

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { errorHandler, NotFoundError, currentUser } from '@ktltickets/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +16,7 @@ app.use(
   })
 );
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
